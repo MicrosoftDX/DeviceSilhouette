b11a84defda33a5aee7c3ffb4340a5307c7aa670
Thomas Conte <thomas@conte.com>
2016-05-10 16:06:16 +0200

    Server-side test process

---
+++ src/client/node/test/EventProcessor/EventProcessor/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iotHubConnectionString = "HostName=ciscohackhub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=1WDxwhY0q0MXW8zhdVUjZFDEMGgjT/+Q0wmVmphtT9E=";
+            string iotHubD2cEndpoint = "messages/events";
+            TestEventProcessor.StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=hubbhub;AccountKey=CHc0MrCFtNtGrK0l9JsDONeKThGWn+O07PUrZsceHXkCuV1nJtO7DGMZwQK8O3reD6jEJIqPQ9UBs5bH40eMdg==";
+            ServiceClient serviceClient;
+
+            // The Service Client is used to send messages to the device (C2D)
+            serviceClient = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+
+            // Start the Event Processor Host to receive messages from the devices (D2C)
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, iotHubD2cEndpoint,
+                EventHubConsumerGroup.DefaultGroupName, iotHubConnectionString, TestEventProcessor.StorageConnectionString, "test-silhouette-events");
+
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<TestEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. q to quit, g to get state, u to update state");
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+
+                if (cmd == "q")
+                    break;
+
+                // Create message -- should not be empty! Otherwise does not trigger on Node.JS client side.
+                // TODO: request delivery feedback?
+                var commandMessage = new Message(Encoding.ASCII.GetBytes("{}"));
+
+                switch (cmd)
+                {
+                    case "g":
+                        Console.WriteLine("Sending C2D_GetState...");
+                        commandMessage.Properties.Add("MessageType", "C2D_GetState");
+                        serviceClient.SendAsync("silhouette1", commandMessage);
+                        break;
+                    case "u":
+                        Console.WriteLine("Sending C2D_UpdateState...");
+                        commandMessage.Properties.Add("MessageType", "C2D_UpdateState");
+                        serviceClient.SendAsync("silhouette1", commandMessage);
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command.");
+                        break;
+                }
+            }
+
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
---
+++ src/client/node/test/EventProcessor/EventProcessor/TestEventProcessor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventProcessor
+{
+    class TestEventProcessor : IEventProcessor
+    {
+        public static string StorageConnectionString;
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
+        }
+
+        public async Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine("EventProcessor initialized.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (EventData eventData in messages)
+            {
+                byte[] data = eventData.GetBytes();
+                string deviceId = eventData.SystemProperties["iothub-connection-device-id"].ToString();
+                string messageType = eventData.Properties.Keys.Contains("MessageType") ? eventData.Properties["MessageType"].ToString() : "null";
+
+                Console.WriteLine(string.Format("Message received.  Partition: '{0}', DeviceID: '{1}', MessageType: '{2}', Data: '{3}'",
+                    context.Lease.PartitionId, deviceId, messageType, Encoding.UTF8.GetString(data)));
+
+                // Dispatch message
+                switch (messageType)
+                {
+                    case "D2C_UpdateState":
+                        await ProcessD2CUpdateState(deviceId, Encoding.UTF8.GetString(data));
+                        break;
+                    case "D2C_GetState":
+                        await ProcessD2CGetState(deviceId, Encoding.UTF8.GetString(data));
+                        break;
+                    default:
+                        Console.WriteLine("Unknown MessageType.");
+                        break;
+                }
+            }
+        }
+
+        public async Task ProcessD2CUpdateState(string deviceId, string data)
+        {
+            Console.WriteLine("Update State");
+        }
+
+        public async Task ProcessD2CGetState(string deviceId, string data)
+        {
+            Console.WriteLine("Get State");
+        }
+
+    }
+}
