16cae03d27cc340c2018ba11ab24d25fb6563ed9
Astrid Hackenberg <astrid.hackenberg@live.nl>
2016-05-25 12:50:42 +0200

    Added Repository Actor and test console app

---
+++ Services/StateManagementService/DeviceRepository/DeviceRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Actors.Client;
+using DeviceRepository.Interfaces;
+using DeviceStateNamespace;
+
+namespace DeviceRepository
+{
+    /// <remarks>
+    /// This class represents an actor.
+    /// Every ActorID maps to an instance of this class.
+    /// The StatePersistence attribute determines persistence and replication of actor state:
+    ///  - Persisted: State is written to disk and replicated.
+    ///  - Volatile: State is kept in memory only and replicated.
+    ///  - None: State is kept in memory only and not replicated.
+    /// </remarks>
+    [StatePersistence(StatePersistence.Persisted)]
+    internal class DeviceRepositoryActor : Actor, IDeviceRepositoryActor
+    {
+        public Task<DeviceState> GetDeviceStateAsync()
+        {
+            return this.StateManager.GetStateAsync<DeviceState>("silhouette");
+        }
+
+        public Task SetDeviceStateAsync(DeviceState state)
+        {
+            return this.StateManager.SetStateAsync("silhouette", state);
+        }
+
+        /// <summary>
+        /// This method is called whenever an actor is activated.
+        /// An actor is activated the first time any of its methods are invoked.
+        /// </summary>
+        protected override Task OnActivateAsync()
+        {
+            ActorEventSource.Current.ActorMessage(this, "Actor activated.");
+
+            // The StateManager is this actor's private state store.
+            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
+            // Any serializable object can be saved in the StateManager.
+            // For more information, see http://aka.ms/servicefabricactorsstateserialization
+
+            return this.StateManager.TryAddStateAsync("silhouette", new DeviceState());
+        }
+
+    }
+}
