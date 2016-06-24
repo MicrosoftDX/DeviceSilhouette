710fb758d55cd2f356dd3c4f03ec192f935d8a04
Stuart Leeks <stuartle@microsoft.com>
2016-06-24 10:48:28 +0100

    remove test code

--- Services/StateManagementService/StateManagementService/Scripts/Override-Parameters.ps1
+++ Services/StateManagementService/StateManagementService/Scripts/Override-Parameters.ps1
@@ -2,7 +2,6 @@ function CreateParametersFileWithEnvironmentOverrides($paramsFilename)
 {
     $xml = [xml](Get-Content $paramsFilename)
     $xml.Application.Parameters.Parameter | %{ 
-        # [PSCustomObject]@{ "Name"= $_.Name; "EnvVarExists" = (Test-Path "env:$($_.Name)")} 
         $parameterName = $_.Name
         if (Test-Path "env:$parameterName"){
             $newValue = (Get-Item "env:$parameterName").Value
