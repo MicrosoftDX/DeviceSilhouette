0d9b0fe02ac4e4d8f752e3589385817f365fed17
Stuart Leeks <stuartle@microsoft.com>
2016-07-13 11:42:29 +0100

    fix up post-build step

--- Services/StateManagementService/StateManagementService/Scripts/InitializeGeneratedParametersFiles.ps1
+++ Services/StateManagementService/StateManagementService/Scripts/InitializeGeneratedParametersFiles.ps1
@@ -1,4 +1,6 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+Write-Host "Removing *-generated.xml from '$here'"
 Remove-Item "$here\..\ApplicationParameters\*-generated.xml"
 
+Write-Host "Recreating *-generated.xml in '$here'"
 Get-ChildItem "$here\..\ApplicationParameters\*.xml" | %{copy-item "$($_.FullName)" "$($_.FullName.Replace(".xml", "-generated.xml"))"}
