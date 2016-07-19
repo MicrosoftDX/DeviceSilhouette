8f5aecf0e8d0fd0cfc4d820d4b9bf0a90398631b
astridhack <astridha@microsoft.com>
2016-07-19 08:43:51 +0200

    Added projects and classes for State historie provider

---
+++ Services/StateManagementService/StorageProviders/BlobStorage/BlobStorageProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeviceRichState;
+
+namespace StorageProviders.BlobStorage
+{
+    class BlobStorageProvider : IHistoryStorage
+    {
+        private string _storageConnection;
+        private const string _storageContainer = "silhouette-messages";
+
+        public BlobStorageProvider(string storageConnection)
+        {
+            _storageConnection = storageConnection;
+        }
+
+        /// <summary>
+        /// Stores a single state message to blob storage
+        /// </summary>
+        /// <param name="stateMessage">the device state message to store</param>
+        /// <returns></returns>
+        public Task StoreStateMessage(DeviceState stateMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Stores a collection of state messages to blob storage
+        /// </summary>
+        /// <param name="stateMessages">device state messages to store</param>
+        /// <returns></returns>
+        public Task StoreStateMessages(DeviceState[] stateMessages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/StateManagementService/StorageProviders/IStorageProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeviceRichState;
+
+namespace StorageProviders
+{
+    public interface IHistoryStorage
+    {
+        Task StoreStateMessage(DeviceState stateMessage);
+
+        Task StoreStateMessages(DeviceState[] stateMessages);
+    }
+
+}
