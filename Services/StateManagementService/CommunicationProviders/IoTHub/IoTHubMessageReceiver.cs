b3240d9a341a784f03c4a6ed2084a4efe7f3e063
Stuart Leeks <stuartle@microsoft.com>
2016-07-25 11:04:25 +0100

    Split IoTHub sender receiver into separate files

--- Services/StateManagementService/CommunicationProviders/IoTHub/IoTHubMessageReceiver.cs
+++ Services/StateManagementService/CommunicationProviders/IoTHub/IoTHubMessageReceiver.cs
@@ -42,32 +42,6 @@ namespace CommunicationProviders.IoTHub
             cancellationToken.WaitHandle.WaitOne();
         }
     }
-
-    public class IotHubMessageSender : IMessageSender
-    {
-        private readonly ServiceClient _serviceClient;
-
-        public IotHubMessageSender(string iotHubConnectionString)
-        {
-            _serviceClient = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
-        }
-        public async Task SendCloudToDeviceAsync(DeviceMessage silhouetteMessage)
-        {
-            var commandMessage = new Message(System.Text.Encoding.UTF8.GetBytes(silhouetteMessage.Values))
-            {
-                Properties = {
-                    { "MessageType", silhouetteMessage.MessageType.ToString() },
-                    { "MessageSubType", silhouetteMessage.MessageSubType.ToString() }
-                },
-                // get full acknowledgement on message delivery
-                Ack = DeliveryAcknowledgement.Full,
-                ExpiryTimeUtc = DateTime.UtcNow.AddMilliseconds(silhouetteMessage.MessageTtlMs),
-                CorrelationId = silhouetteMessage.CorrelationId
-            };
-
-            await _serviceClient.SendAsync(silhouetteMessage.DeviceId, commandMessage);
-        }
-    }
 }
 
 
---
+++ Services/StateManagementService/CommunicationProviders/IoTHub/IoTHubMessageSender.cs
@@ -0,0 +1,38 @@
+﻿using DeviceRichState;
+using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommunicationProviders.IoTHub
+{
+    public class IotHubMessageSender : IMessageSender
+    {
+        private readonly ServiceClient _serviceClient;
+
+        public IotHubMessageSender(string iotHubConnectionString)
+        {
+            _serviceClient = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
+        }
+        public async Task SendCloudToDeviceAsync(DeviceMessage silhouetteMessage)
+        {
+            var commandMessage = new Message(System.Text.Encoding.UTF8.GetBytes(silhouetteMessage.Values))
+            {
+                Properties = {
+                    { "MessageType", silhouetteMessage.MessageType.ToString() },
+                    { "MessageSubType", silhouetteMessage.MessageSubType.ToString() }
+                },
+                // get full acknowledgement on message delivery
+                Ack = DeliveryAcknowledgement.Full,
+                ExpiryTimeUtc = DateTime.UtcNow.AddMilliseconds(silhouetteMessage.MessageTtlMs),
+                CorrelationId = silhouetteMessage.CorrelationId
+            };
+
+            await _serviceClient.SendAsync(silhouetteMessage.DeviceId, commandMessage);
+        }
+    }
+}
+
+
+
