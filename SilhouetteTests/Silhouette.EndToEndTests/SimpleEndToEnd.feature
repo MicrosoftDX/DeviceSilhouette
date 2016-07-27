44d871c8a6af0feda52b8e2b4ede9b05dee2b639
Stuart Leeks <stuartle@microsoft.com>
2016-07-27 12:31:31 +0100

    Working on SpecFlow

---
+++ SilhouetteTests/Silhouette.EndToEndTests/SimpleEndToEnd.feature
@@ -0,0 +1,9 @@
+﻿Feature: SimpleEndToEnd
+	Ensure the basic cloud-to-device and device-to-cloud functionality is working
+
+@mytag
+Scenario: Device to cloud messages
+	Given a registered and connected device with id e2eDevice1
+	When the device reports its state
+	Then the reported state API should contain the reported state 
+	And the messages API should contain the reported state message
---
+++ SilhouetteTests/Silhouette.EndToEndTests/SimpleEndToEndSteps.cs
@@ -0,0 +1,53 @@
+﻿using DotNetClient;
+using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Silhouette.EndToEndTests
+{
+    [Binding]
+    public class SimpleEndToEndSteps
+    {
+        [Given]
+        public void Given_a_registered_and_connected_device_with_id_DEVICEID(string deviceId)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When]
+        public void When_the_device_reports_its_state()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void Then_the_reported_state_API_should_contain_the_reported_state()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void Then_the_messages_API_should_contain_the_reported_state_message()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+        private async Task<DeviceSimulator> GetDeviceAsync(string deviceId)
+        {
+            const string templateConnectionString = "%Silhouette_IotHubConnectionString%";
+            string connectionString = Environment.ExpandEnvironmentVariables(templateConnectionString);
+            if (connectionString == templateConnectionString)
+            {
+                throw new Exception("Ensure that the Silhouette_IotHubConnectionString environment variable is set");
+            }
+
+            var device = new DeviceSimulator(connectionString, deviceId);
+
+            await device.InitializeAsync();
+
+            return device;
+        }
+
+    }
+}
