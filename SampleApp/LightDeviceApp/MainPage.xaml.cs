dadfa071ee9185cf8901e3bbbbc699288bcde86a
navalev <navaisman@gmail.com>
2016-08-16 16:05:32 +0300

    Update Home Light simulator
    1. Add device connection string input for user
    2. Add a connection page before starting the simulation

--- SampleApp/LightDeviceApp/MainPage.xaml.cs
+++ SampleApp/LightDeviceApp/MainPage.xaml.cs
@@ -31,134 +31,25 @@ namespace LightDeviceApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private bool sensorActivated = false;
-        CancellationTokenSource _sensorCancellationTokenSource = new CancellationTokenSource();
-
+        
         public MainPage()
         {
-            this.InitializeComponent();
-           
-            Task.Run(
-                 async () => {
-                     while (true)
-                     {                         
-                         var message = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
-                         
-                         await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => {                             
-                             proccesMessage(message);                             
-                         });                         
-                     }
-                 }
-                 );
-
+            this.InitializeComponent();                                   
         }
 
         
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
         {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            string connectionString = connectStr.Text;
+            if (connectionString != null)
             {
-                while (true)
-                {
-                    reportState();
-                    await Task.Delay(TimeSpan.FromSeconds(10));
-                }
-                
-            });
-        }
-
-        private void proccesMessage(Message message)
-        {
-            if (message == null) return;
-                     
-            var messageData = Encoding.ASCII.GetString(message.GetBytes());
-            incomingMessage.Text = "Last incoming message: " + messageData;            
-
-            // TODO: add error handling
-            string messageType = message.Properties["MessageType"];
-            string messageSubType = message.Properties["MessageSubType"];
-
-            if (messageType.Equals("CommandRequest"))
-            {
-                switch (messageSubType)
-                {
-                    case "SetState":
-                        setState(messageData);
-                        break;
-                    case "LatestState":
-                        break;
-                    case "ReportState":
-                        reportState();
-                        break;
-                }
+                AzureIoTHub.CreateDeviceClient(connectionString);
+                connectButton.IsEnabled = false;
+                connectStr.IsEnabled = false;
+                MyFrame.Navigate(typeof(HomeLightPage));
             }
-            else if (messageType.Equals("InquiryResponse") && messageSubType.Equals("GetState")) {
-                reportState();
-            }
+            
         }
-
-        private void reportState()
-        {
-            if (toggleSwitch != null)
-            {
-                DeviceMessage msg = new DeviceMessage(toggleSwitch.IsOn);
-                string json = JsonConvert.SerializeObject(msg);
-                AzureIoTHub.SendDeviceToCloudMessageAsync(json, "Report", "State");
-                outgoingMessage.Text = "Last outgoing message: " + json;
-            }
-        }
-
-        private void setState(string messageData)
-        {
-            // expect the message data to hold a state for the light - on or off
-            // "{"status" : "on"}"           
-            DeviceMessage msg = JsonConvert.DeserializeObject<DeviceMessage>(messageData);
-            toggleSwitch.IsOn = msg.status.Equals("on");
-        }
-
-        private void toggleSwitch_Toggled(object sender, RoutedEventArgs e)
-        {
-            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
-            if (toggleSwitch != null)
-            {
-                reportState();            
-            }
-        }
-
-        private async void button_Click(object sender, RoutedEventArgs e)
-        {
-            if (!sensorActivated)
-            {
-                button.Content = "Stop Sensor";
-                sensorActivated = true;
-                _sensorCancellationTokenSource = new CancellationTokenSource();
-                // simulate light sensor changing the light status every 5 minutes
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
-                {
-                    while (!_sensorCancellationTokenSource.IsCancellationRequested)
-                    {
-                        await Task.Delay(TimeSpan.FromMinutes(5));
-                        toggleSwitch.IsOn = !toggleSwitch.IsOn;
-                    }
-                });
-            }
-            else
-            {
-                button.Content = "Start Sensor";
-                sensorActivated = false;
-                _sensorCancellationTokenSource.Cancel();
-            }
-        }
-      
-    }
-
-    internal class DeviceMessage
-    {
-        public string status;
-
-        public DeviceMessage(bool isOn)
-        {
-            status = isOn ? "on" : "off";
-        }
-    }
+    }    
 }
